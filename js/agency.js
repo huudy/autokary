2642fbf8670e24633bbf2a1572f3e8ccb5817fe9
Trenda Paweł (UniCredit Business Integrated Solutions) <C314446@hd00.UNICREDITGROUP.EU>
2017-12-15 15:36:13 +0100

    video bg and fixed offset

--- js/agency.js
+++ js/agency.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
+          scrollTop: (target.offset().top)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 54
+    offset: 0
   });
 
   // Collapse Navbar
@@ -54,5 +54,13 @@
   setTimeout(function(){
     loadingSpinner();
   }, 2000);
+
+  //animated bg
+  $('.masthead').vide({
+    mp4: "mp4/bg.mp4",
+    poster: "img/bg-mobile-fallback.jpg"
+  }, {
+    posterType: 'jpg'
+  });
 
 })(jQuery); // End of use strict
