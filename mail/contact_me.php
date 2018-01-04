52c4e6b4816d74bf63d2d4b5a9d58b417b95f707
huudy <treendaa@gmail.com>
2018-01-04 17:36:25 +0100

    working mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,8 +19,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'biuro@wiktoria-autokary.pl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers = "From: biuro@wiktoria-autokary.pl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";  
+$headers .= "To: biuro@wiktoria-autokary.pl";  
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
